f1453849d3e079857cc10fa9b63ca066138eb26f
Elijah Spina, PhD <ejspina@gmail.com>
2023-10-01 14:10:29 -0700

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const appName: string = process.env.NEXT_PUBLIC_APP_TITLE || "MINTBASE";
+const appName: string = process.env.NEXT_PUBLIC_APP_TITLE || "Cannsta";
 const network = process.env.NEXT_PUBLIC_NETWORK || "testnet";
 const proxyContractAddress =
   process.env.NEXT_PUBLIC_PROXY_MINTER_CONTRACT_ADDRESS ||
