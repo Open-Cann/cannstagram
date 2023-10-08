fcec369f5139db8e9ea285399bbffb005323cc77
Elijah Spina, PhD <ejspina@gmail.com>
2023-10-08 15:26:34 -0700

    Update camera.tsx
    fix deploy error

--- src/components/pages/camera.tsx
+++ src/components/pages/camera.tsx
@@ -28,46 +28,43 @@ export default function CameraPage() {
   ]; 
   const overlayMask = () => {
     // Get the current image index from state
-    const currentImageIndex = imageIndex !== null ? imageIndex : -1;
-    const nextImageIndex = (currentImageIndex + 1) % (images.length + 1); // Add 1 to the array length
-  
-    // Remove the currently displayed image
-    const currentImage = document.getElementById("image-overlay");
-    if (currentImage) {
-      currentImage.remove();
-    }
-  
-    // If nextImageIndex is within the array bounds, display the next image overlay
-    if (nextImageIndex < images.length) {
-      const img = document.createElement("img");
-      img.src = images[nextImageIndex];
-      img.style.position = "absolute";
-      img.width = 290; // Set the desired width
-      img.height = 290; // Set the desired height
-      img.style.top = "50%";
-      img.style.left = "50%";
-      img.style.transform = "translate(-50%, -50%)";
-      img.style.zIndex = "9999";
-      img.id = "image-overlay"; // Set an ID for easy removal
-      document.body.appendChild(img);
-  
-      // Update the image index in state
-      setImageIndex(nextImageIndex);
-  
-      // Remove the image after 15 seconds (15000 milliseconds)
-      setTimeout(() => {
-        // Check if img element exists before removing it
-        const currentImage = document.getElementById("image-overlay");
-        if (currentImage) {
-          currentImage.remove();
-          setImageIndex(null); // Reset the image index
-        }
-      }, 15000);
-    } else {
-      // If nextImageIndex is out of bounds, set imageIndex to null to prevent further cycling
-      setImageIndex(null);
-    }
-  };
+  const currentImageIndex = imageIndex !== null ? imageIndex : -1;
+  const nextImageIndex = (currentImageIndex + 1) % images.length;
+
+  // Remove the currently displayed image
+  const currentImage = document.getElementById("image-overlay");
+  if (currentImage) {
+    currentImage.remove();
+  }
+
+  // If nextImageIndex is within the array bounds, display the next image overlay
+  if (nextImageIndex < images.length) {
+    const img = document.createElement("img");
+    img.src = images[nextImageIndex];
+    img.style.position = "absolute";
+    img.width = 290; // Set the desired width
+    img.height = 290; // Set the desired height
+    img.style.top = "50%";
+    img.style.left = "50%";
+    img.style.transform = "translate(-50%, -50%)";
+    img.style.zIndex = "9999";
+    img.id = "image-overlay"; // Set an ID for easy removal
+    document.body.appendChild(img);
+
+    // Update the image index in state
+    setImageIndex(nextImageIndex as null);
+
+    // Remove the image after 15 seconds (15000 milliseconds)
+    setTimeout(() => {
+      // Check if img element exists before removing it
+      const currentImage = document.getElementById("image-overlay");
+      if (currentImage) {
+        currentImage.remove();
+        setImageIndex(null); // Reset the image index
+      }
+    }, 15000);
+  }
+};
 
   if (picture) {
     return <Mint currentPhoto={picture} backStep={tryAgain} />;
