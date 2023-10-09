563d91021042d3bde5407909214ee4c3d07207f6
Elijah Spina <ejspina@gmail.com>
2023-10-08 21:31:43 -0700

    fix

--- src/components/pages/camera.tsx
+++ src/components/pages/camera.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Mint } from "@/components/Mint";
 import { FooterButton } from "@/components/footer";
 import Webcam from "react-webcam";
@@ -19,52 +18,75 @@ export default function CameraPage() {
     capture,
   } = useCamera();
 
-  const [imageIndex, setImageIndex] = useState(null); // State to keep track of the current image index
+  const [imageIndex, setImageIndex] = useState<number | null>(null);
   const images = [
     "https://i.ibb.co/3vd5dYS/Open-Cann-Icon-2023.png",
     "https://i.ibb.co/x2KvnKd/cannabisgenome-e-Noun-removebg-preview.png",
     "https://i.ibb.co/sR34j87/OIP-removebg-preview.png"
-    // Add more image filenames to the array as needed
-  ]; 
+  ];
+
+  const [countdown, setCountdown] = useState(20); // Initial countdown value in seconds
+  const [countdownIntervalId, setCountdownIntervalId] = useState<NodeJS.Timeout | null>(null);
+
   const overlayMask = () => {
-    // Get the current image index from state
-  const currentImageIndex = imageIndex !== null ? imageIndex : -1;
-  const nextImageIndex = (currentImageIndex + 1) % images.length;
-
-  // Remove the currently displayed image
-  const currentImage = document.getElementById("image-overlay");
-  if (currentImage) {
-    currentImage.remove();
-  }
-
-  // If nextImageIndex is within the array bounds, display the next image overlay
-  if (nextImageIndex < images.length) {
+    const currentImageIndex = imageIndex !== null ? imageIndex : -1;
+    const nextImageIndex = (currentImageIndex + 1) % images.length;
+  
+    const currentImage = document.getElementById("image-overlay");
+    if (currentImage) {
+      currentImage.remove();
+    }
+  
     const img = document.createElement("img");
     img.src = images[nextImageIndex];
     img.style.position = "absolute";
-    img.width = 290; // Set the desired width
-    img.height = 290; // Set the desired height
+    img.width = 290;
+    img.height = 290;
     img.style.top = "50%";
     img.style.left = "50%";
     img.style.transform = "translate(-50%, -50%)";
     img.style.zIndex = "9999";
-    img.id = "image-overlay"; // Set an ID for easy removal
+    img.id = "image-overlay";
     document.body.appendChild(img);
+  
+    setImageIndex(nextImageIndex);
+    setCountdown(20); // Reset the countdown timer to 20 seconds
+  
+    // Clear the previous interval before starting a new one
+    if (countdownIntervalId) {
+      clearInterval(countdownIntervalId);
+    }
+  
+    const intervalId = setInterval(() => {
+      setCountdown(prevCountdown => {
+        if (prevCountdown <= 1) {
+          clearInterval(intervalId); // Clear the interval when countdown reaches 0
+          const currentImage = document.getElementById("image-overlay");
+          if (currentImage) {
+            currentImage.remove();
+            setImageIndex(null);
+          }
+          return 0;
+        }
+        return prevCountdown - 1;
+      });
+    }, 1000); // Set the interval to 1000 milliseconds (1 second)
+  
+    // Store the interval ID for cleanup
+    setCountdownIntervalId(intervalId);
+  };
+  
+  
+  
 
-    // Update the image index in state
-    setImageIndex(nextImageIndex as unknown);
-
-    // Remove the image after 10 seconds (10000 milliseconds)
-    setTimeout(() => {
-      // Check if img element exists before removing it
-      const currentImage = document.getElementById("image-overlay");
-      if (currentImage) {
-        currentImage.remove();
-        setImageIndex(null); // Reset the image index
+  // Cleanup countdown interval when component unmounts
+  useEffect(() => {
+    return () => {
+      if (countdownIntervalId) {
+        clearInterval(countdownIntervalId);
       }
-    }, 10000);
-  }
-};
+    };
+  }, [countdownIntervalId]);
 
   if (picture) {
     return <Mint currentPhoto={picture} backStep={tryAgain} />;
@@ -72,7 +94,7 @@ export default function CameraPage() {
 
   return (
     <>
-      <main className="h-camera overflow-hidden	 w-screen flex items-center justify-center">
+      <main className="h-camera overflow-hidden w-screen flex items-center justify-center">
         <div className="h-1/2 relative m-camera">
           {!!cameraLoaded && (
             <h2 className="align-center flex font-semibold mb-4 text-mainText text-center">
@@ -85,10 +107,6 @@ export default function CameraPage() {
               </div>
             </h2>
           )}
-          {/* 
-            {pageLoaded && !permissionGranted && (
-            <button onClick={requestCameraPermission}>Try Again</button>
-          )} */}
 
           {permissionGranted && !picture && (
             <Webcam
@@ -108,9 +126,20 @@ export default function CameraPage() {
               }}
             />
           )}
+
+          {/* Button container with absolute positioning */}
+          <div className="fixed bottom-100 left-0 w-full flex justify-center p-4">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={overlayMask}
+          >
+            Add Overlay
+          </button>
+          {imageIndex !== null && <p className="text-black ml-4">{`Overlay will disappear in ${countdown} seconds`}</p>}
+        </div>
         </div>
       </main>
-          
+
       {!picture && (
         <footer className="fixed bottom-0 left-0 flex w-full items-end justify-center bg-primary h-16">
           <FooterButton onClick={capture} />
